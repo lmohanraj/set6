4eeb2ed19e854433cff59329f74406d53f1b9643
lmohanraj <35128040+lmohanraj@users.noreply.github.com>
2018-02-08 19:25:37 +0530

    Create nearestevennum.c

---
+++ nearestevennum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+	int r,n ;
+	printf("input");
+	scanf("%d",&n);
+              if(n%2==0)
+              r=n;
+              else
+              r=n-1;
+	 printf(" output=%d",r);
+	return 0;
+}
