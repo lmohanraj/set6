aea11e8a41aa20fd2b5446491c1f58b7c00acaef
lmohanraj <35128040+lmohanraj@users.noreply.github.com>
2018-02-08 19:56:56 +0530

    Update alpph&numinstring.c

--- alpph&numinstring.c
+++ alpph&numinstring.c
@@ -1,24 +1,28 @@
-#include<stdio.h>
+ #include<stdio.h>
 #include<string.h>
 int main()
 {
 	int n,i,b=0,d=0;
-	char a[100];
-	printf("input");
+	char a[1000];
+	printf("input\n");
             gets(a);
             n=strlen(a);
 	for(i=0;i<n;i++)
 	{
-		if('a'<=a[i]>='z')
+		if(a[i]>='a'&&a[i]<='z')
 		b++;
-		if('A'<=a[i]>='Z')
+		if(a[i]>='A'&&a[i]<='Z')
 	            b++;
-		if('0'<=a[i]>='9')
-		d++
+		if(a[i]>='0'&&a[i]<='9')
+		d++;
 	}
-              if(b!=0&&d!=0)
+              if((b!=0)&&(d!=0))
+              {
 	 printf(" yes");
+              }
 	 else
+	 {
 	 printf("no");
+	 }
 	return 0;
 }
