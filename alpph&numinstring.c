83c6da19d1f13f460ba870729ecb8375f1712c6e
lmohanraj <35128040+lmohanraj@users.noreply.github.com>
2018-02-08 19:46:01 +0530

    Create alpph&numinstring.c

---
+++ alpph&numinstring.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int n,i,b=0,d=0;
+	char a[100];
+	printf("input");
+            gets(a);
+            n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		if('a'<=a[i]>='z')
+		b++;
+		if('A'<=a[i]>='Z')
+	            b++;
+		if('0'<=a[i]>='9')
+		d++
+	}
+              if(b!=0&&d!=0)
+	 printf(" yes");
+	 else
+	 printf("no");
+	return 0;
+}
