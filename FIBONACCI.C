75a396de8a335ae271ab473814bb53687c23e9f0
lmohanraj <35128040+lmohanraj@users.noreply.github.com>
2018-02-08 20:13:36 +0530

    Update FIBONACCI.C

--- FIBONACCI.C
+++ FIBONACCI.C
@@ -4,14 +4,13 @@ int main()
 	int n,a=1,b=1,i,t;
 	printf("enter LIMIT");
 	scanf("%d",&n);
- printf("\t%d\t%d",a,b);
+         printf("\t%d\t%d",a,b);
 	for(i=0;i<n-2;i++)
 	{
-t=a+b;
-printf("\t%d",t);
-a=b;
-b=t;
+          t=a+b;
+          printf("\t%d",t);
+           a=b;
+           b=t;
 	}
-
 	return 0;
 }
